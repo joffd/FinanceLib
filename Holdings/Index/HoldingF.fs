5431f54eb3db7253c64c95fe6082eb78433c3a45
joffrey.dalet@hotmail.com <joffrey.dalet@hotmail.com>
2020-07-23 03:28:43 +0800

    fixed HoldingF

--- Holdings/Index/HoldingF.fs
+++ Holdings/Index/HoldingF.fs
@@ -14,11 +14,11 @@ module HoldingF =
         ?account: string, ?trader: string) =
         
         interface IHolding with
-            member _.Security = f :> ISecurity
+            member _.Security = Security.F f
             member _.MarketDataEnv = marketdataenv
             member _.PricingEngine = pricingEngineF :> IPricingEngine
             member _.FxPair = 
-                FxPair.createFxPair (fst qd) (getCurrency (f :> ISecurity).Underlying) (snd qd)
+                FxPair.createFxPair (snd qd) (getCurrency (f :> ISecurity).Underlying) (fst qd)
             member _.Account = account
             member _.Trader = trader
 
